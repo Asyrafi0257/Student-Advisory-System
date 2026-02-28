953ce40f47d04547bdadbe250886cdc97f40fc7c
Alif Asyrafi <asyrafi0257@gmail.com>
2026-03-01 04:01:30 +0800

    new updated

--- student-advisory/src/app/api/auth/login/route.js
+++ student-advisory/src/app/api/auth/login/route.js
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
 import pool from "@/lib/db";
+import bcrypt, { hash } from "bcrypt";
 
 export async function POST(req){
     try{
         const {name, password} = await req.json();
-        console.log("Input :" + name, password)
+
+        //nak hash password
+        const hashPassword = await bcrypt.hash(password, 10);
 
         const [rows] = await pool.query(
-            "SELECT * FROM tbl_admin WHERE admin_name = ? AND admin_password = ?", [name, password]
+            "SELECT * FROM tbl_admin WHERE admin_name = ?", [name]
         );
         
-        if(rows.length > 0){
-            return NextResponse.json({success:true});
+        if(rows.length == 0){
+            return NextResponse.json({success:false});
         } 
 
-        return NextResponse.json({success: false});
+        const isMatch = await bcrypt.compare(password, rows[0].admin_password);
+        if(!isMatch){
+            return NextResponse.json({success:false});
+        }
+
+        return NextResponse.json({success:true})
 
     } catch(error) {
         return NextResponse.json({error:error.message});
