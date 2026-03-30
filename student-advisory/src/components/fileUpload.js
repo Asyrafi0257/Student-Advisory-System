273908520f75d8dde964dc4c333bff5a03e4b1d0
Alif Asyrafi <asyrafi0257@gmail.com>
2026-03-30 16:55:02 +0800

    feat: add search button for find file

--- student-advisory/src/components/fileUpload.js
+++ student-advisory/src/components/fileUpload.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { FileSpreadsheet } from "lucide-react";
+import { FileSpreadsheet, Search } from "lucide-react";
 
 
 export default function FileUpload() {
@@ -23,9 +23,16 @@ export default function FileUpload() {
     }
     return (
         <div className="bg-[#ffffff] shadow-md rounded-xl h-[300px] p-4 md:p-6">
-            <div className="flex border-b border-gray-300 pb-3">
+            <div className="flex flex-row border-b border-gray-300 pb-3 justify-between">
               <h3 className="font-bold text-[24px]">Attached Files</h3>
+              <div className="relative w-sm">
+                <input type="text" placeholder="search..." className="w-full h-[40px] shadow-sm rounded-lg border-1 border-gray-200 px-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-black-300"/>
+                <button className="absolute top-1/2 -translate-y-1/2 right-3 cursor-pointer">
+                    <Search className="text-gray-400"/>
+                </button>
+              </div>
             </div>
+
             <div>
                 {files.length === 0 ? ( 
                     <p>No Files found</p>
