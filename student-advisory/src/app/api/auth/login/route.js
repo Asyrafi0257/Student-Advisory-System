fc34f69520f79d1a4d51a811e5277e307e1c3671
Asyrafi0257 <asyrafi0257@gmail.com>
2026-06-03 17:28:18 +0800

    fixed: still solve problem login

--- student-advisory/src/app/api/auth/login/route.js
+++ student-advisory/src/app/api/auth/login/route.js
@@ -133,7 +133,7 @@ export async function POST(req) {
 
         response.cookies.set("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "lax",
             path: "/",
             maxAge: 60 * 60 * 24
