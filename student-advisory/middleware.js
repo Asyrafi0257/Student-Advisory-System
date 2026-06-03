5ba206b81d813b36e04b5f6b59a00911839cfcbd
Asyrafi0257 <asyrafi0257@gmail.com>
2026-06-04 01:12:12 +0800

    fixed:fix problem token

--- student-advisory/middleware.js
+++ student-advisory/middleware.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { verifyToken } from "@/lib/jwt";
 
+export const runtime = "nodejs";
+
 export function middleware(req) {
 
     // ================= DEBUG =================
